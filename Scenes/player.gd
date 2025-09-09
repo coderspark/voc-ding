0754cf3c5ad2406c0cdca41ad6ef6e313a349bae
Coderspark <luke@slaughter.com>
2025-09-09 16:58:56 +0200

    no more wave bounce

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,15 +1,13 @@
 extends CharacterBody2D
 
 var speed = 100
-var offset = 0
 
 func _ready() -> void:
 	$Navigation.target_position = position
 
 func _process(delta: float) -> void:
-	offset += 0.1
 	var dir = to_local($Navigation.get_next_path_position()).normalized()
-	velocity = dir * 1100 * delta + Vector2(0, cos(offset)) * 25
+	velocity = dir * 1100 * delta
 	if dir.x >= 0:
 		$Sprite.flip_h = true
 	else:
