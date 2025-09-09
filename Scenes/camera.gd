1cfcf05ff9d86ae602a984fe0d1ae5e6ab19aeb8
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2025-09-09 16:54:16 +0200

    created fog-based exploration
    (and camera zoom

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+var camera_speed := 4	
+
 func _process(delta: float) -> void:
-	position += Vector2(Input.get_axis("left","right"),Input.get_axis("up","down"))
+	position += (Vector2(Input.get_axis("left","right"),Input.get_axis("up","down")) * (Vector2(1,1) / $Camera.zoom)) * camera_speed
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			$Camera.zoom *= 0.9
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			$Camera.zoom *= 1.1
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -15,7 +15,19 @@ func _process(delta: float) -> void:
 	else:
 		$Sprite.flip_h = false
 	move_and_slide()
+	for cell in get_cells_around_player($"../Fog".local_to_map(global_position)):
+		$"../Fog".erase_cell(cell)
+	
 
 
 func _on_terrain_click(pos:Vector2) -> void:
 	$Navigation.target_position = pos
+
+func get_cells_around_player(pos:Vector2i):
+	var out = []
+	for n in $"../Fog".get_surrounding_cells(pos):
+		for x in  $"../Fog".get_surrounding_cells(n):
+			for y in  $"../Fog".get_surrounding_cells(x):
+				if not y in out:
+					out.append(y)
+	return out
