bfd00e5042cdb4b1121a0ab92bc1dd6641e3c0b0
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2025-09-10 12:23:37 +0200

    Camera moveable with right click

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -3,10 +3,10 @@ extends Node2D
 var camera_speed := 4	
 var right_clicked = false
 var start_pos : Vector2
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if right_clicked:
-		position += get_global_mouse_position() - start_pos
-	position += (Vector2(Input.get_axis("left","right"),Input.get_axis("up","down")) * (Vector2(1,1) / $Camera.zoom)) * camera_speed
+		position = start_pos - get_local_mouse_position()
+	#position += (Vector2(Input.get_axis("left","right"),Input.get_axis("up","down")) * (Vector2(1,1) / $Camera.zoom)) * camera_speed
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -20,6 +20,6 @@ func _input(event: InputEvent) -> void:
 				$Camera.zoom = Vector2(15,15)
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
 			right_clicked = true
-			start_pos = get_global_mouse_position()
+			start_pos = position + get_local_mouse_position()
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_released():
 			right_clicked = false
