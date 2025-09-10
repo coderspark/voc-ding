6f89e33b37c657acb4afd7cb94de8569a239e17a
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2025-09-10 14:51:56 +0200

    text rendering ui

---
+++ Code/dialogue.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func show_dialogue(text:String,character:String):
+	$CanvasLayer/Text.text = ""
+	for char in text:
+		$Timer.start()
+		await $Timer.timeout
+		$CanvasLayer/Text.text += char
+
+func _ready() -> void:
+	show_dialogue("Hello there, fellow traveler!","Mary")
