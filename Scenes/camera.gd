c554d53175c7106712eaf2376e49bcd3937a0ad7
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2025-09-10 12:04:09 +0200

    fixed ship rotation

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 var camera_speed := 4	
-
+var right_clicked = false
+var start_pos : Vector2
 func _process(delta: float) -> void:
+	if right_clicked:
+		position += get_global_mouse_position() - start_pos
 	position += (Vector2(Input.get_axis("left","right"),Input.get_axis("up","down")) * (Vector2(1,1) / $Camera.zoom)) * camera_speed
 
 func _input(event: InputEvent) -> void:
@@ -15,3 +18,8 @@ func _input(event: InputEvent) -> void:
 			$Camera.zoom *= 1.1
 			if $Camera.zoom.x > 15:
 				$Camera.zoom = Vector2(15,15)
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
+			right_clicked = true
+			start_pos = get_global_mouse_position()
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_released():
+			right_clicked = false
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,26 +2,44 @@ extends CharacterBody2D
 
 var speed = 100
 
+var on_water = true
+var move = false
+var target : Vector2
+
+var old_dir_x = 0
+
 func _ready() -> void:
 	$Navigation.target_position = position
-
-func _process(delta: float) -> void:
-	var dir = to_local($Navigation.get_next_path_position()).normalized()
-	velocity = dir * 1100 * delta
-	if dir.x >= 0:
-		$Sprite.flip_h = true
-	else:
-		$Sprite.flip_h = false
-	move_and_slide()
 	var a = get_cells_around_player($"../Fog".local_to_map(global_position))
 	for cell in a.keys():
-		print(a[cell])
 		if a[cell] == 0:
 			$"../Fog".erase_cell(cell)
 		elif not $"../Fog".get_cell_atlas_coords(cell) == Vector2i(-1,-1):
 			$"../Fog".set_cell(cell,0,Vector2i(15,1))
+
+func _process(delta: float) -> void:
+	if move:
+		var dir = to_local($Navigation.get_next_path_position()).normalized()
+		velocity = dir * 1100 * delta
+		print(dir.x)
+		if dir.x > 0:
+			if dir.x * old_dir_x > 0:
+				$Sprite.flip_h = true
+		else:
+			if dir.x * old_dir_x > 0:
+				$Sprite.flip_h = false
+		old_dir_x = dir.x
+		move_and_slide()
+		var a = get_cells_around_player($"../Fog".local_to_map(global_position))
+		for cell in a.keys():
+			if a[cell] == 0:
+				$"../Fog".erase_cell(cell)
+			elif not $"../Fog".get_cell_atlas_coords(cell) == Vector2i(-1,-1):
+				$"../Fog".set_cell(cell,0,Vector2i(15,1))
 func _on_terrain_click(pos:Vector2) -> void:
 	$Navigation.target_position = pos
+	target = pos
+	move = true
 
 func get_cells_around_player(pos:Vector2i):
 	var out = {}
@@ -33,3 +51,12 @@ func get_cells_around_player(pos:Vector2i):
 				if not y in out:
 					out.set(y,1)
 	return out
+
+
+func _on_navigation_target_reached() -> void:
+	if move:
+		print($Navigation.distance_to_target() * 0.07)
+		$Timer.start($Navigation.distance_to_target() * 0.07)
+		await $Timer.timeout
+		print("stop")
+		move = false
