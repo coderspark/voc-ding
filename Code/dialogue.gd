6606683a3249827539f79ef39bfbccca8082ccdb
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2025-09-11 12:34:52 +0200

    Better dialogue system

--- Code/dialogue.gd
+++ Code/dialogue.gd
@@ -1,12 +1,17 @@
 extends Control
 
-func show_dialogue(text:String,character:String):
-	$CanvasLayer/Text.text = ""
-	for char in text:
-		$Timer.start()
+func show_dialogue(data:Array):
+	$CanvasLayer.show()
+	for n in data:
+		$CanvasLayer/Text.text = ""
+		$CanvasLayer/Name.text = n[1]
+		for char in n[0]:
+			$Timer.start(0.03)
+			await $Timer.timeout
+			$CanvasLayer/Text.text += char
+		$Timer.start(1)
 		await $Timer.timeout
-		$CanvasLayer/Text.text += char
+	$CanvasLayer.hide()
 
 func _ready() -> void:
-	show_dialogue("Hello there, fellow traveler!","Mary")
-	show_dialogue("Kill yourself, NOW!","Mary")
+	show_dialogue([["hey!","mary"],["hey!","john"],["kill yourself!","mary"],["not if i kill you first!","john"],["I died!","mary"]])
